cfd504abfb37d426f2de7fd299e6b6f81176c452
Kaimaru <kuyama2018@gmail.com>
2025-10-24 23:08:23 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.GITHUB_ACTIONS ? '/kais-room' : '',
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/kais-room' : '',
   trailingSlash: true,
 };
 
