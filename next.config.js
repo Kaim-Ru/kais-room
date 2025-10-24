008ecd0aec2cf4f23c0bebb76bf781bf140709cd
Kaimaru <kuyama2018@gmail.com>
2025-10-24 23:19:15 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/kais-room' : '';
+
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? '/kais-room' : '',
-  assetPrefix: process.env.GITHUB_ACTIONS ? '/kais-room' : '',
+  basePath: basePath,
+  assetPrefix: basePath,
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
